537ee721b69a171c49042af31e57ec872eac41ae
yerimkoko <gochi97@naver.com>
2021-11-24 03:14:52 +0900

    [ST-5] feat: ApiResponse 설정

--- streetvendor-api/src/main/java/store/streetvendor/controller/TestController.java
+++ streetvendor-api/src/main/java/store/streetvendor/controller/TestController.java
@@ -2,13 +2,14 @@ package store.streetvendor.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import store.streetvendor.controller.dto.ApiResponse;
 
 @RestController
 public class TestController {
 
     @GetMapping("/ping")
-    public String test() {
-        return "pong";
+    public ApiResponse<String> test() {
+        return ApiResponse.success("pong");
     }
 
 }
---
+++ streetvendor-api/src/main/java/store/streetvendor/controller/dto/ApiResponse.java
@@ -0,0 +1,25 @@
+package store.streetvendor.controller.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class ApiResponse<T> {
+
+    public static final ApiResponse<String> OK = new ApiResponse<>("", "", "");
+
+    private final String code;
+
+    private final String message;
+
+    private final T data;
+
+    public static <T> ApiResponse<T> success(T data) {
+        return new ApiResponse<>("", "", data);
+    }
+
+    public static ApiResponse<Object> error(String code, String message) {
+        return new ApiResponse<>(code, message, null);
+    }
+}
