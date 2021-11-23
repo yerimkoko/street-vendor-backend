d904baa79e075d9c6ccd20e17b7bcdd023720c66
seungh0 <will.seungho@gmail.com>
2021-11-24 03:02:20 +0900

    [ST-4] fix: .editorconfig 추가 및 컨벤션 설정

--- streetvendor-api/src/main/java/store/streetvendor/controller/TestController.java
+++ streetvendor-api/src/main/java/store/streetvendor/controller/TestController.java
@@ -5,9 +5,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-    @GetMapping("/ping")
-    public String test() {
-        return "pong";
-    }
+
+	@GetMapping("/ping")
+	public String test() {
+		return "pong";
+	}
 
 }
