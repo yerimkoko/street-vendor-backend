f131b2e329a04618c629b91e1e6b7344f9ec7417
yerimkoko <gochi97@naver.com>
2021-11-27 23:20:31 +0900

    [ST-16] feat: save session

---
+++ streetvendor-api/src/main/java/store/streetvendor/config/HttpSessionConfig.java
@@ -0,0 +1,8 @@
+package store.streetvendor.config;
+
+import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
+
+@EnableJdbcHttpSession
+public class HttpSessionConfig {
+
+}
