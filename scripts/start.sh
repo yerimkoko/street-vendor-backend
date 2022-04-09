6cf6448c7b3786738129ef3c396f50aea30d1cee
yerimkoko <gochi97@naver.com>
2022-04-09 16:51:33 +0900

    [ST-47] feat: script 추가

---
+++ scripts/start.sh
@@ -0,0 +1,1 @@
+nohup java -jar /home/ubuntu/street-vendor-backend/streetvendor-api/build/libs/streetvendor-api.jar &
---
+++ scripts/stop.sh
@@ -0,0 +1,1 @@
+kill `ps -ef | grep "streetvendor-api.jar" | grep -v grep | awk '{print $2}'`
