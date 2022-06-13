857602f7a539f716c7aa9e0d112d3760c306b279
yerimkoko <gochi97@naver.com>
2022-06-14 00:03:28 +0900

    [ST-116] feat: admin module

---
+++ streetvendor-admin/src/main/java/com/example/streetvendoradmin/StreetvendorAdminApplication.java
@@ -0,0 +1,13 @@
+package com.example.streetvendoradmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreetvendorAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreetvendorAdminApplication.class, args);
+    }
+
+}
---
+++ streetvendor-admin/src/test/java/com/example/streetvendoradmin/StreetvendorAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.streetvendoradmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreetvendorAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
