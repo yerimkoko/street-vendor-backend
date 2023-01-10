15366a8020f4ca1f4db4051b443b29c63368190e
yerimkoko <gochi97@naver.com>
2023-01-10 15:28:37 +0900

    [ST-199] fix: 오타 수정

--- scripts/boss-start.sh
+++ scripts/boss-start.sh
@@ -1,1 +1,1 @@
-nohup java -Dspring.profiles.active=dev =jar /home/ubuntu/street-vendor-backend/streetvendor-boss/build/libs/streetvendor-boss.jar &
+nohup java -Dspring.profiles.active=dev -jar /home/ubuntu/street-vendor-backend/streetvendor-boss/build/libs/streetvendor-boss.jar &
