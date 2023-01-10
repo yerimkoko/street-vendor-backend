0686772314290fe22596d4f866590f5a5307f514
yerimkoko <gochi97@naver.com>
2023-01-10 14:34:29 +0900

    [ST-199] fix: boss, admin 서버 설정

---
+++ scripts/boss-start.sh
@@ -0,0 +1,1 @@
+nohup java -Dspring.profiles.active=dev =jar /home/ubuntu/street-vendor-backend/streetvendor-boss/build/libs/streetvendor-boss.jar &
---
+++ scripts/boss-stop.sh
@@ -0,0 +1,1 @@
+kill `ps -ef | grep "streetvendor-boss.jar" | grep -v grep | awk '{print $2}'`
