cae434de91e554f518fda10d1c466e3a03250938
yerimkoko <gochi97@naver.com>
2023-01-15 11:30:25 +0900

    [ST-204] fix: 에러코드

--- streetvendor-boss/src/main/java/store/streetvendor/service/BossService.java
+++ streetvendor-boss/src/main/java/store/streetvendor/service/BossService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import store.streetvendor.core.domain.boss.Boss;
 import store.streetvendor.core.domain.boss.BossRepository;
-import store.streetvendor.core.exception.ConflictException;
+import store.streetvendor.core.exception.DuplicatedException;
 import store.streetvendor.service.dto.request.BossSignUpRequest;
 
 @RequiredArgsConstructor
@@ -25,14 +25,14 @@ public class BossService {
     private void validateEmail(String email) {
         Boss boss = bossRepository.findByEmail(email);
         if (boss != null) {
-            throw new ConflictException(String.format("[%s]는 이미 존재하는 boss 입니다. 기존 아이디로 로그인해주세요.", email));
+            throw new DuplicatedException(String.format("[%s]는 이미 존재하는 boss 입니다. 기존 아이디로 로그인해주세요.", email));
         }
     }
 
     private void validateNickName(String nickName) {
         Boss boss = bossRepository.findByNickName(nickName);
         if (boss != null) {
-            throw new ConflictException(String.format("[%s]는 이미 존재하는 닉네임 입니다. 다른 닉네임을 입력해주세요.", nickName));
+            throw new DuplicatedException(String.format("[%s]는 이미 존재하는 닉네임 입니다. 다른 닉네임을 입력해주세요.", nickName));
         }
     }
 }
