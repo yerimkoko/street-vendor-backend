b3bdb8d1d29b32c6a09cea4b4e68c96b60cfc2dd
yerimkoko <gochi97@naver.com>
2023-01-17 19:16:50 +0900

    [ST-209] fix: validate admin

--- streetvendor-admin/src/main/java/store/streetvendor/service/AdminService.java
+++ streetvendor-admin/src/main/java/store/streetvendor/service/AdminService.java
@@ -3,6 +3,7 @@ package store.streetvendor.service;
 import store.streetvendor.core.config.auth.dto.request.AuthRequest;
 import store.streetvendor.core.config.auth.dto.response.AuthResponse;
 import store.streetvendor.core.exception.ConflictException;
+import store.streetvendor.core.exception.DuplicatedException;
 import store.streetvendor.core.exception.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -63,14 +64,14 @@ public class AdminService {
     private void validateEmail(String email) {
         Admin admin = adminRepository.findByEmail(email);
         if (admin != null) {
-            throw new ConflictException(String.format("[%s]는 이미 존재하는 이메일 입니다.", email));
+            throw new DuplicatedException(String.format("[%s]는 이미 존재하는 이메일 입니다.", email));
         }
     }
 
     private void validateNickName(String nickName) {
         Admin admin = adminRepository.findByNickName(nickName);
         if (admin != null) {
-            throw new ConflictException(String.format("[%s]는 이미 존재하는 닉네임입니다. 다른 닉네임을 입력해주세요"));
+            throw new DuplicatedException(String.format("[%s]는 이미 존재하는 닉네임입니다. 다른 닉네임을 입력해주세요"));
         }
     }
 
