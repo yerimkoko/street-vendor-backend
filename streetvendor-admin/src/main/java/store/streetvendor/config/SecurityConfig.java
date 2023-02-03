345c3f33b336be6e98b99a538a7337dc762c6500
yerimkoko <gochi97@naver.com>
2023-02-03 23:15:26 +0900

    [ST-210] hotfix: cors 설정

--- streetvendor-admin/src/main/java/store/streetvendor/config/SecurityConfig.java
+++ streetvendor-admin/src/main/java/store/streetvendor/config/SecurityConfig.java
@@ -1,13 +1,10 @@
 package store.streetvendor.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -29,11 +26,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(WebSecurity web) throws Exception{
         web.ignoring().antMatchers("/h2-console/**");
     }
-
-    @Bean
-    public PasswordEncoder getPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
 
 }
